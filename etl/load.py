49d87e6c1048bfc681cad40595632ba7fe5985ca
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2025-06-02 13:49:31 +1000

    adding a download function

--- etl/load.py
+++ etl/load.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from transform import transform
+from datetime import datetime
 
 # настройка логирования
 logging.basicConfig(
@@ -12,3 +14,20 @@ logging.basicConfig(
 #  параметры подключения
 DSN = os.getenv(
     "ETL_DSN", "dbname=etl_pipline user=user password=root host=localhost port=5432")
+
+
+def load_etl():
+    # функция загрузки: берем данные с transform и заполняем таблицы
+    logging.info("Запускаем этап LOAD...")
+    records = transform()
+    if not records:
+        logging.warning("Transform вернул пустой список. Нечего загружать.")
+        return
+
+    # парсим etl_time
+    etl_time_str = records[0]["etl_time"]
+    try:
+        etl_time = datetime.strptime(etl_time_str, "%Y-%m-%d %H:%M:%S")
+    except ValueError as e:
+        logging.error("Неправильный формат etl_time: %s", etl_time_str)
+        raise
